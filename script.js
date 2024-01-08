eee98523f8067b1389cf02e04f4bad3cf79766de
AnzalnaKainat <anzalnakainat@gmail.com>
2024-01-08 21:27:58 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+//////////////////////// Navigation Links///////////////////////////////
+function goToSignUp() {
+  window.location.href = "signup.html";
+}
+
+function goTologin() {
+  window.location.href = "login.html";
+}
+
+function goToHomepage() {
+  window.location.href = "homepage.html";
+}
+
+function goToConfirmedReservation() {
+  window.location.href = "confirmed-reservation.html";
+}
+
+function navigateBackToReservation() {
+  window.location.href = "reservation.html";
+}
+
+function goBackToHomepage() {
+  window.location.href = "homepage.html";
+}
+
+//////////////////Navigation---JavaScript to toggle the mobile menu///////////////////////////
+function showMenu() {
+  var navLinks = document.getElementById("navLinks");
+  navLinks.classList.toggle("active");
+}
+
+function hideMenu() {
+  var navLinks = document.getElementById("navLinks");
+  navLinks.classList.remove("active");
+}
+
+////////////////////Search Toggle///////////////////////////
+function toggleSearch() {
+  const searchLabel = document.getElementById("search");
+  if (
+    searchLabel.style.dispaly === "none" ||
+    searchLabel.style.display === ""
+  ) {
+    searchLabel.style.display = "block";
+  } else {
+    searchLabel.style.display = "none";
+  }
+}
